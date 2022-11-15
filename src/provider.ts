6cbfe73642c5aa75a7f9ae5fda94c50b9c3a5b76
benStre <strehle.benedikt@gmail.com>
2022-11-15 22:47:41 +0100

    fix deno import problems

--- src/provider.ts
+++ src/provider.ts
@@ -5,15 +5,15 @@ import { App } from "./app.ts";
 
 await Datex.Supranet.connect();
 
-// UIX.Provider.use(()=>new UIX.Components.GridGroup({rows:[1,1], columns:[1,2], border_color:'var(--blue)'}, {margin:20}, [
-// 	new UIX.Components.TextView({text:"Hi from Server", border_color:'var(--green)', border:5}, {margin:20}),
-// 	new UIX.Components.TextView({text:"Hi from Server 2", border_color:'var(--red)', border:5}, {margin:20, gx:1})
-// ]))
-UIX.Provider.use(App);
+UIX.Provider.use(()=>new UIX.Components.GridGroup({rows:[1,1], columns:[1,2], border_color:'var(--blue)'}, {margin:20}, [
+	new UIX.Components.TextView({text:"Hi from Server", border_color:'var(--green)', border:5}, {margin:20}),
+	new UIX.Components.TextView({text:"Hi from Server 2", border_color:'var(--red)', border:5}, {margin:20, gx:1})
+]))
+// UIX.Provider.use(App);
 
 
 UIX.Provider.setRootDir(new URL("./public/", import.meta.url));
-UIX.Provider.setImportMap(new URL("./public/importmap.json", import.meta.url))
+UIX.Provider.setImportMap(new URL("./importmap.json", import.meta.url))
 UIX.Provider.useFile('./main.js')
 
 UIX.Provider.goPublic();
