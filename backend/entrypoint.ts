2b7b7547bb0689f2641db53ea208ee21ccc128d6
Jonas Strehle <jonas@strehles.info>
2024-10-08 21:19:25 +0200

    Fix imports, use dev importmap (temporary)

--- backend/entrypoint.ts
+++ backend/entrypoint.ts
@@ -3,7 +3,8 @@
  * This module provides a default export that defines the UI that is returned from the backend
  * when a page is visited
  */
-import { provideRedirect } from "uix/html/entrypoint-providers.tsx"
+import { provideRedirect } from "uix/providers/common.tsx";
+import { type Entrypoint } from "uix/providers/entrypoints.ts";
 
 export default {
 	// show backend (hybrid) rendered page on /backend
@@ -11,4 +12,4 @@ export default {
 	
 	// redirect / to /backend
 	'/': provideRedirect("/backend")
-}
+} satisfies Entrypoint;
